f09a2413fbaf68e18b269a5879a30cdce9fa9a06
aronovgj <aronovgj@gmx.net>
2015-01-05 20:47:02 +0100

    add malwarebytes
    add malwarebytes

---
+++ _output/sumatrapdf.prerelease/3.1.10094/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$ProcessActive = Get-Process sumatraPDF* -ErrorAction SilentlyContinue
+if($ProcessActive -ne $null)
+{
+    Stop-Process -ProcessName sumatraPDF*
+}
+Install-ChocolateyPackage 'sumatrapdf.prerelease' 'exe' '/S' 'http://kjkpub.s3.amazonaws.com/sumatrapdf/prerel/SumatraPDF-prerelease-10094-install.exe' -validExitCodes @(0)
