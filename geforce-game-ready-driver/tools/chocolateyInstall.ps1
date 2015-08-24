76a0deb0ac7ab918a59c08a8b156cfdef96e5757
Tithen-Firion <tithen.firion.0@gmail.com>
2015-08-24 21:02:14 +0200

    support notebook and win 10 drivers
    More about `$osVersion` here in 'Remarks':
    https://msdn.microsoft.com/en-us/library/windows/desktop/ms724833%28v=vs.85%29.aspx
    
    Regex in `Get-GraphicsCardType` matches cards ending with `M`, `M LE`
    and `MX` - everythin listed here:
    http://www.nvidia.com/Download/driverResults.aspx/89120/en-us

--- geforce-game-ready-driver/tools/chocolateyInstall.ps1
+++ geforce-game-ready-driver/tools/chocolateyInstall.ps1
@@ -1,12 +1,41 @@
 ﻿$packageName = '{{PackageName}}'
-$url = '{{DownloadUrl}}'
 $fileType = 'exe'
 $silentArgs = '-s -noreboot'
-$url64 = '{{DownloadUrlx64}}'
+$urlDesktop = '{{DownloadUrl}}'         #0
+$urlDesktopWin10 = '{{DownloadUrlx64}}' #1
+$urlNotebook = '{{Checksum}}'           #2
+$urlNotebookWin10 = '{{Checksumx64}}'   #3
+
+function Get-GraphicsCardType {
+  $cards = @(gwmi win32_VideoController)
+  foreach ($card in $cards) {
+    $name = $card.Name.Trim()
+    if ($name -match 'NVIDIA') { break }
+  }
+  if ($name -match 'M(\s+LE|X)?$') {
+    return 'notebook'
+  } else {
+    return 'desktop'
+  }
+}
 
 $osBitness = Get-ProcessorBits
+$osVersion = [Environment]::OSVersion.Version.Major
+$cardType = Get-GraphicsCardType
+$downloadVersion = 0
+
+if ($osVersion -eq 10)        { $downloadVersion += 1 }
+if ($cardType -eq 'notebook') { $downloadVersion += 2 }
+
+switch ($downloadVersion){
+  3 {$url = $urlNotebookWin10; break}
+  2 {$url = $urlNotebook; break}
+  1 {$url = $urlDesktopWin10; break}
+  default {$url = $urlDesktop; break}
+}
+
 if ($osBitness -eq 64) {
-	$url = $url64
+  $url = $url.Replace('-32bit-', '-64bit-')
 }
 
 new-item "${env:temp}\nvidiadriver" -itemtype directory -Force
