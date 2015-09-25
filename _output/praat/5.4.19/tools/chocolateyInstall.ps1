cfb0811b47f05b91031a409ec7fb9f452e6d20de
aronovgj <aronovgj@gmx.net>
2015-09-26 00:01:47 +0200

    updates
    updates

---
+++ _output/praat/5.4.19/tools/chocolateyInstall.ps1
@@ -0,0 +1,13 @@
+$packageName = 'praat'
+$url = 'http://www.fon.hum.uva.nl/praat/praat5419_win32.zip'
+$url64 = 'http://www.fon.hum.uva.nl/praat/praat5419_win64.zip'
+$fileName = "Praat.exe"
+$linkName = "Praat.lnk"
+$destdir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+Install-ChocolateyZipPackage "$packageName" "$url" "$destdir" "$url64"
+
+#install start menu shortcut
+$programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
+$shortcutFilePath = Join-Path $programs $linkName 
+$targetPath = Join-Path $destdir $fileName
+Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePath -targetPath $targetPath
