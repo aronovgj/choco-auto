d15c3e45162dedda7c7e97fe0bd5b564c8038b2c
aronovgj <aronovgj@gmx.net>
2015-09-26 02:30:38 +0200

    add miktex
    add miktex

---
+++ _output/miktex/2.9.5721/tools/chocolateyInstall.ps1
@@ -0,0 +1,18 @@
+﻿$packageName   = 'miktex'
+$Url = 'http://mirrors.ctan.org/systems/win32/miktex/setup/basic-miktex-2.9.5721.exe'
+$Url64 = 'http://mirrors.ctan.org/systems/win32/miktex/setup/basic-miktex-2.9.5721-x64.exe'
+$fileType = 'EXE'
+$silentArgs = '--unattended --shared'
+$scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+
+$osBitness = Get-ProcessorBits
+if ($osBitness -eq 64) {
+	$filePath = Join-Path $scriptPath 'basic-miktex-2.9.5721-x64.exe'
+}
+else {
+	$filePath = Join-Path $scriptPath 'basic-miktex-2.9.5721.exe'
+}
+
+Get-ChocolateyWebFile $packageName $filePath $Url $Url64
+
+Install-ChocolateyInstallPackage $packageName $fileType $silentArgs $filePath
---
+++ miktex/tools/chocolateyInstall.ps1
@@ -0,0 +1,18 @@
+﻿$packageName   = 'miktex'
+$Url = '{{DownloadUrl}}'
+$Url64 = '{{DownloadUrlx64}}'
+$fileType = 'EXE'
+$silentArgs = '--unattended --shared'
+$scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+
+$osBitness = Get-ProcessorBits
+if ($osBitness -eq 64) {
+	$filePath = Join-Path $scriptPath 'basic-miktex-{{PackageVersion}}-x64.exe'
+}
+else {
+	$filePath = Join-Path $scriptPath 'basic-miktex-{{PackageVersion}}.exe'
+}
+
+Get-ChocolateyWebFile $packageName $filePath $Url $Url64
+
+Install-ChocolateyInstallPackage $packageName $fileType $silentArgs $filePath
