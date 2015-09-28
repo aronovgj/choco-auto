5699434cb51e4b8ffe559376bf8cb7561f959770
Anthony Mastrean <anthony.mastrean@gmail.com>
2015-09-28 13:05:48 -0400

    Address moderation feedback
    I'm not entirely sure how to configure automatic checksum updates. The author doesn't publish them, so you can't scrape them from the web. Does Ketarin allow you to run an arbitrary script as part of the update?

--- miktex/tools/chocolateyInstall.ps1
+++ miktex/tools/chocolateyInstall.ps1
@@ -1,18 +1,26 @@
-﻿$packageName   = 'miktex'
-$Url = '{{DownloadUrl}}'
-$Url64 = '{{DownloadUrlx64}}'
-$fileType = 'EXE'
-$silentArgs = '--unattended --shared'
-$scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$id = 'miktex'
 
-$osBitness = Get-ProcessorBits
-if ($osBitness -eq 64) {
-	$filePath = Join-Path $scriptPath 'basic-miktex-{{PackageVersion}}-x64.exe'
-}
-else {
-	$filePath = Join-Path $scriptPath 'basic-miktex-{{PackageVersion}}.exe'
-}
+$tools = Split-Path -parent $MyInvocation.MyCommand.Definition
 
-Get-ChocolateyWebFile $packageName $filePath $Url $Url64
+$install32 = Join-Path $tools 'basic-miktex-{{PackageVersion}}.exe'
+$install64 = Join-Path $tools 'basic-miktex-{{PackageVersion}}-x64.exe'
+$install = @{32=$install32;64=$install64}[(Get-ProcessorBits)]
 
-Install-ChocolateyInstallPackage $packageName $fileType $silentArgs $filePath
+# The package installer is very picky about its own file name and silent 
+# installation. See for more information:
+#
+# https://github.com/AnthonyMastrean/chocolateypackages/issues/143#issuecomment-143379145
+#
+Get-ChocolateyWebFile `
+	-PackageName $id `
+	-FileFullPath $install `
+	-Url '{{DownloadUrl}}' `
+	-Url64 '{{DownloadUrlx64}}' `
+	-Checksum '{{Checksum}}' `
+	-Checksum64 '{{Checksum64}}'
+
+Install-ChocolateyInstallPackage `
+	-PackageName $id `
+	-FileType 'EXE' `
+	-SilentArgs '--unattended --shared' `
+	-File $install
