70212255a1517f121551bc52715f3191445c733f
aronovgj <aronovgj@gmx.net>
2015-09-30 20:51:29 +0200

    clearer miktex install script, also updates
    clearer miktex install script, also updates.
    thanks to AnthonyMastrean

--- _output/miktex/2.9.5721/tools/chocolateyInstall.ps1
+++ _output/miktex/2.9.5721/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName   = 'miktex'
-$Url = 'http://mirrors.ctan.org/systems/win32/miktex/setup/basic-miktex-2.9.5721.exe'
-$Url64 = 'http://mirrors.ctan.org/systems/win32/miktex/setup/basic-miktex-2.9.5721-x64.exe'
+$Url = 'http://ctan.math.washington.edu/tex-archive/systems/win32/miktex/setup/basic-miktex-2.9.5721.exe'
+$Url64 = 'http://ctan.math.washington.edu/tex-archive/systems/win32/miktex/setup/basic-miktex-2.9.5721-x64.exe'
 $fileType = 'EXE'
 $silentArgs = '--unattended --shared'
 $scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
@@ -16,3 +16,5 @@ else {
 Get-ChocolateyWebFile $packageName $filePath $Url $Url64
 
 Install-ChocolateyInstallPackage $packageName $fileType $silentArgs $filePath
+
+Remove-Item $filePath -Force
--- miktex/tools/chocolateyInstall.ps1
+++ miktex/tools/chocolateyInstall.ps1
@@ -5,14 +5,20 @@ $fileType = 'EXE'
 $silentArgs = '--unattended --shared'
 $scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 
-$osBitness = Get-ProcessorBits
-if ($osBitness -eq 64) {
-	$filePath = Join-Path $scriptPath 'basic-miktex-{{PackageVersion}}-x64.exe'
-}
-else {
-	$filePath = Join-Path $scriptPath 'basic-miktex-{{PackageVersion}}.exe'
-}
+$install32 = Join-Path $scriptPath 'basic-miktex-{{PackageVersion}}.exe'
+$install64 = Join-Path $scriptPath 'basic-miktex-{{PackageVersion}}-x64.exe'
+$filePath = @{32=$install32;64=$install64}[(Get-ProcessorBits)]
+
+
+# The package installer is very picky about its own file name and silent 
+# installation. See for more information:
+# https://github.com/AnthonyMastrean/chocolateypackages/issues/143#issuecomment-143379145
+# 
+# will be fixed when issue is resolved:
+# https://github.com/chocolatey/choco/issues/435
 
 Get-ChocolateyWebFile $packageName $filePath $Url $Url64
 
 Install-ChocolateyInstallPackage $packageName $fileType $silentArgs $filePath
+
+Remove-Item $filePath -Force
