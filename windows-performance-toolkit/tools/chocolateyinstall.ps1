13761080539b04994371513381f8aee9104de3c5
aronovgj <aronovgj@gmx.net>
2015-11-08 02:45:16 +0100

    add windows performance toolkit, updates
    add windows performance toolkit, updates

---
+++ windows-performance-toolkit/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+$packageName= 'windows-performance-toolkit'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://download.microsoft.com/download/B/0/C/B0C80BA3-8AD6-4958-810B-6882485230B5/standalonesdk/sdksetup.exe' 
+
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'EXE' 
+  silentArgs    = '/Features OptionId.WindowsPerformanceToolkit /norestart /quiet /ceip off'
+  url           = $url
+}
+
+Install-ChocolateyPackage @packageArgs
