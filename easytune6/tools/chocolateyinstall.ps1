87464437f47c0ca829505c521ac5c067c848bfac
aronovgj <aronovgj@gmx.net>
2016-01-03 19:04:14 +0100

    add easyune, updates
    add easyune, updates

---
+++ easytune6/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop'; 
+
+$packageName= '{{PackageName}}'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = '{{DownloadUrl}}'
+$archiveLocation = Join-Path $toolsDir 'easytune.zip'
+$folderLocation = Join-Path $toolsDir 'Easytune6'
+$fileLocation = Join-Path $folderLocation 'setup.exe'
+$silentArgs = '/S /v/qn'
+$fileType = 'exe'
+
+Get-ChocolateyWebFile $packageName $archiveLocation $url
+Get-ChocolateyUnzip $archiveLocation $toolsDir
+Install-ChocolateyInstallPackage $packageName $fileType $silentArgs $fileLocation 
+Remove-Item $fileLocation -Force
+Remove-Item $archiveLocation -Force
---
+++ google-books-downloader/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop';
+$packageName= '{{PackageName}}'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = '{{DownloadUrl}}' 
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'EXE'
+  silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  url           = $url
+}
+
+Install-ChocolateyPackage @packageArgs
