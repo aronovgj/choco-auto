82222cba61318fa4144d38863000d12794ca9ad8
Link <link.satonaka@outlook.com>
2016-03-19 17:51:57 -0400

    forcibly exclude GeforceExperience installation, pretty up variables

--- geforce-game-ready-driver/tools/chocolateyInstall.ps1
+++ geforce-game-ready-driver/tools/chocolateyInstall.ps1
@@ -2,6 +2,9 @@
 $version = '{{PackageVersion}}'
 $fileType = 'exe'
 $silentArgs = '-s -noreboot'
+$unpackDir = New-Item "${ENV:TEMP}\nvidiadriver" -ItemType Directory -Force
+$unpackFile = Join-Path "$unpackDir" "nvidiadriver.zip"
+$setupFile = Join-Path "$unpackDir" "setup.exe"
 
 $WindowsVersion = (Get-WmiObject -Class Win32_OperatingSystem).Caption
 if ( $WindowsVersion -match 'Windows 10' ) {
@@ -13,10 +16,10 @@ Else {
 	$url64 = "http://us.download.nvidia.com/Windows/$version/$version-desktop-win8-win7-winvista-64bit-international-whql.exe"
 }
 
-$unpackDir = New-Item "${ENV:TEMP}\nvidiadriver" -ItemType Directory -Force
-$unpackFile = Join-Path "$unpackDir" "nvidiadriver.zip"
 Get-ChocolateyWebFile $packageName $unpackFile $url $url64
 Get-ChocolateyUnzip $unpackFile $unpackDir
-$file = Join-Path "$unpackDir" "setup.exe"
-Install-ChocolateyInstallPackage $packageName $fileType $silentArgs $file 
+Remove-Item $unpackDir\Update.Core -Recurse -Force
+Remove-Item $unpackDir\ShadowPlay -Recurse -Force
+Remove-Item $unpackDir\GFExperience* -Recurse -Force
+Install-ChocolateyInstallPackage $packageName $fileType $silentArgs $setupFile
 Remove-Item $unpackDir -Recurse -Force
