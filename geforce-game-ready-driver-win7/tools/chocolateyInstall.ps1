e5a3d248b549a87ab3c6a021e4dde68948a4af2b
aronovgj <aronovgj@gmx.net>
2016-10-23 19:18:46 +0200

    Nvidia stopped packaging crapware into their installers
    Nvidia stopped packaging crapware into their installers
    Also make download links clickable on .org.

--- geforce-game-ready-driver-win7/tools/chocolateyInstall.ps1
+++ geforce-game-ready-driver-win7/tools/chocolateyInstall.ps1
@@ -6,14 +6,14 @@ $unpackDir = New-Item "${ENV:TEMP}\nvidiadriver" -ItemType Directory -Force
 $unpackFile = "${ENV:TEMP}\nvidiadriver.zip"
 $setupFile = Join-Path "$unpackDir" "setup.exe"
 
-$url = "http://us.download.nvidia.com/Windows/$version/$version-desktop-win8-win7-32bit-international-whql.exe"
-$url64 = "http://us.download.nvidia.com/Windows/$version/$version-desktop-win8-win7-64bit-international-whql.exe"
+$url = "http://us.download.nvidia.com/Windows/{{PackageVersion}}/{{PackageVersion}}-desktop-win8-win7-32bit-international-whql.exe"
+$url64 = "http://us.download.nvidia.com/Windows/{{PackageVersion}}/{{PackageVersion}}-desktop-win8-win7-64bit-international-whql.exe"
 
 
 Get-ChocolateyWebFile $packageName $unpackFile $url $url64 -Checksum {{checksum}} -ChecksumType 'sha256' -Checksum64 {{checksumx64}} -ChecksumType64 'sha256'
 Get-ChocolateyUnzip $unpackFile $unpackDir
-Remove-Item $unpackDir\Update.Core -Recurse -Force
-Remove-Item $unpackDir\Display.Update -Recurse -Force
-Remove-Item $unpackDir\ShadowPlay -Recurse -Force
+#Remove-Item $unpackDir\Update.Core -Recurse -Force
+#Remove-Item $unpackDir\Display.Update -Recurse -Force
+#Remove-Item $unpackDir\ShadowPlay -Recurse -Force
 Install-ChocolateyInstallPackage $packageName $fileType $silentArgs $setupFile
 Remove-Item $unpackDir -Recurse -Force
