ead576fb366e62d7da7a6e4efcee92a19754e49e
aronovgj <aronovgj@gmx.net>
2016-10-25 23:11:01 +0200

    Make JDownloader "official"
    Make JDownloader "official"

---
+++ jdownloader/tools/chocolateyInstall.ps1
@@ -0,0 +1,18 @@
+﻿$packageName = 'jdownloader'
+$url = 'http://installer.jdownloader.org/ic/JD2SilentSetup_x86.exe'
+$url64 = 'http://installer.jdownloader.org/ic/JD2SilentSetup_x64.exe'
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'EXE'
+  silentArgs    = '-q'
+  url           = $url
+  url64bit		= $url64
+  checksum 		= '{{checksum}}'
+  checksumType 	= 'sha256'
+  checksum64     = '{{checksumx64}}'
+  checksumType64 = 'sha256'
+  
+}
+
+Install-ChocolateyPackage @packageArgs
