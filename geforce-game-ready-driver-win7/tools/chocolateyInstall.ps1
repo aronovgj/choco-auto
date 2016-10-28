e6614bc94f94f115415d8183ebefd33cde854e2c
aronovgj <aronovgj@gmx.net>
2016-10-28 23:14:46 +0200

    Make link clickable
    Make link clickable

--- geforce-game-ready-driver-win7/tools/chocolateyInstall.ps1
+++ geforce-game-ready-driver-win7/tools/chocolateyInstall.ps1
@@ -6,8 +6,8 @@ $unpackDir = New-Item "${ENV:TEMP}\nvidiadriver" -ItemType Directory -Force
 $unpackFile = "${ENV:TEMP}\nvidiadriver.zip"
 $setupFile = Join-Path "$unpackDir" "setup.exe"
 
-$url = "http://us.download.nvidia.com/Windows/$version/$version-desktop-win8-win7-32bit-international-whql.exe"
-$url64 = "http://us.download.nvidia.com/Windows/$version/$version-desktop-win8-win7-64bit-international-whql.exe"
+$url = "http://us.download.nvidia.com/Windows/{{PackageVersion}}/{{PackageVersion}}-desktop-win8-win7-32bit-international-whql.exe"
+$url64 = "http://us.download.nvidia.com/Windows/{{PackageVersion}}/{{PackageVersion}}-desktop-win8-win7-64bit-international-whql.exe"
 
 
 Get-ChocolateyWebFile $packageName $unpackFile $url $url64 -Checksum {{checksum}} -ChecksumType 'sha256' -Checksum64 {{checksumx64}} -ChecksumType64 'sha256'
