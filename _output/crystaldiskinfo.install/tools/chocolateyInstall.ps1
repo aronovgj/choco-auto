d813735be0c21c51b7de6a7331bd72388b15bc13
aronovgj <aronovgj@gmx.net>
2016-11-01 00:43:24 +0100

    updates
    updates

---
+++ _output/crystaldiskinfo.install/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+$packageName = 'crystaldiskinfo.install'
+$url = 'http://rwthaachen.dl.osdn.jp/crystaldiskinfo/65980/CrystalDiskInfo7_0_4-en.exe' 
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'exe'
+  url           = $url
+  silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  softwareName  = $packageName
+  checksum      = '1800282d848c5136d29fa28435e5151e05f797f19094fca9c294e4c2d10cf9af'
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
+
