8b91616a9c3eaaeb9a98602bc62fc3681ed9f1a5
aronovgj <aronovgj@gmx.net>
2017-01-02 20:35:46 +0100

    Remove unused Rainmeter variables
    Remove unused Rainmeter variables

--- rainmeter/tools/chocolateyInstall.ps1
+++ rainmeter/tools/chocolateyInstall.ps1
@@ -1,11 +1,9 @@
 $packageName = '{{PackageName}}'
 $url = '{{DownloadUrl}}'
-$silentArgs = '/S /PORTABLE=0 /STARTUP=1'
-$osBitness = Get-ProcessorBits
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $ahkFile = "$toolsDir\button.ahk"
 $fileType = 'exe'
 
 Start-Process 'AutoHotkey' $ahkFile
 
-Install-ChocolateyPackage $packageName $fileType $silentArgs $url -Checksum {{checksum}} -ChecksumType 'sha256'
+Install-ChocolateyPackage $packageName $fileType $url -Checksum {{checksum}} -ChecksumType 'sha256'
