656812ff95828a2cb23a7a5d9c6a4d395d5bd10f
aronovgj <aronovgj@gmx.net>
2017-01-03 21:31:30 +0100

    updates
    updates

--- _output/rainmeter/tools/button.ahk
+++ _output/rainmeter/tools/button.ahk
@@ -1,3 +1,7 @@
+; For first install. When updating there is no Button1
+WinWait, ahk_class #32770, , 300
+ControlClick, Button1, ahk_class #32770
+
 WinWait, ahk_class #32770, , 300
 ControlClick, Button2, ahk_class #32770
 
@@ -6,6 +10,7 @@ ControlClick, Button2, ahk_class #32770
 
 WinGetTitle, TitleName
 
+; wait for button2 to become active
 Loop {
      Sleep 200
      ControlGet bEnabled, Enabled,, Button2, %TitleName%
--- _output/rainmeter/tools/chocolateyInstall.ps1
+++ _output/rainmeter/tools/chocolateyInstall.ps1
@@ -1,11 +1,17 @@
 $packageName = 'rainmeter'
 $url = 'https://github.com/rainmeter/rainmeter/releases/download/v4.0.0.2746/Rainmeter-4.0.exe'
-$silentArgs = '/S /PORTABLE=0 /STARTUP=1'
-$osBitness = Get-ProcessorBits
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $ahkFile = "$toolsDir\button.ahk"
-$fileType = 'exe'
 
 Start-Process 'AutoHotkey' $ahkFile
 
-Install-ChocolateyPackage $packageName $fileType $silentArgs $url -Checksum 31b9d58b65c6ffac9450c33469dbec2e6463b6855b6d917daa8ee51c5d6d4753 -ChecksumType 'sha256'
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'EXE'
+  silentArgs    = ''
+  url           = $url
+  checksum 		= '31b9d58b65c6ffac9450c33469dbec2e6463b6855b6d917daa8ee51c5d6d4753'
+  ChecksumType 	= 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
