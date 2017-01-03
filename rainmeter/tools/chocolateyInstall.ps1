07d5a657605b4482addcc86f6a5ad8cd945f8905
aronovgj <aronovgj@gmx.net>
2017-01-03 21:31:21 +0100

    Fixing rainmeter
    Fixing rainmeter

--- rainmeter/tools/chocolateyInstall.ps1
+++ rainmeter/tools/chocolateyInstall.ps1
@@ -2,8 +2,16 @@ $packageName = '{{PackageName}}'
 $url = '{{DownloadUrl}}'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $ahkFile = "$toolsDir\button.ahk"
-$fileType = 'exe'
 
 Start-Process 'AutoHotkey' $ahkFile
 
-Install-ChocolateyPackage $packageName $fileType $url -Checksum {{checksum}} -ChecksumType 'sha256'
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'EXE'
+  silentArgs    = ''
+  url           = $url
+  checksum 		= '{{checksum}}'
+  ChecksumType 	= 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
