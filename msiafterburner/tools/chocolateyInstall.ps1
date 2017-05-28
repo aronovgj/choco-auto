ba8ab57f0c206b670a307eda86547fd7f72be5b2
aronovgj <aronovgj@gmx.net>
2017-05-28 20:50:25 +0200

    fix msiafterburner
    fix msiafterburner

--- msiafterburner/tools/chocolateyInstall.ps1
+++ msiafterburner/tools/chocolateyInstall.ps1
@@ -11,7 +11,7 @@ if($ProcessActive -ne $null)
 	Stop-Process -ProcessName MSIAfterburner*
 }
 Get-ChocolateyWebFile $packageName $unpackFile $url -Checksum {{checksum}} -ChecksumType 'sha256'
-Get-ChocolateyUnzip $unpackFile $unpackDir
+Get-ChocolateyUnzip -fileFullPath $unpackFile -destination $unpackDir
 $file = (Get-ChildItem -Path $unpackDir | Where-Object {$_.Name -match "MSIAfterburnerSetup.*.exe$"}).fullname
 
 Install-ChocolateyInstallPackage $packageName $fileType $silentArgs $file 
