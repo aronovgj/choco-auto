936f799c522fb47c707636e7374a4f8b038af90b
aronovgj <aronovgj@gmx.net>
2017-08-01 19:05:44 +0200

    improve ccenhancer install script

--- ccenhancer/tools/chocolateyInstall.ps1
+++ ccenhancer/tools/chocolateyInstall.ps1
@@ -4,22 +4,26 @@ $unzipLocation = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 $fileName = "CCEnhancer.exe"
 $linkName = "CCEnhancer.lnk"
 
-
-$folder = Get-ChildItem $unzipLocation | Where-Object {$_.Name -match "CCEnhancer*"}
+# if upgrading. Else this is null and then it has to be checked again after upgrade.
+$folder = Get-ChildItem $unzipLocation | Where-Object {$_.Name -match "CCEnhancer-.*"}
+$folder = $folder.FullName
 
 If ($folder){
 	If (Test-Path $folder){
-		Remove-Item $folder
+		Remove-Item $folder -Force -Recurse
 	}
 }
 Install-ChocolateyZipPackage $packageName $url $unzipLocation -Checksum {{checksum}} -ChecksumType 'sha256'
 
-$installDir = Join-Path $unzipLocation $folder
-$gui = Join-Path $installDir "CCEnhancer.exe.gui"
+#if fresh install
+$folder = Get-ChildItem $unzipLocation | Where-Object {$_.Name -match "CCEnhancer-.*"}
+$folder = $folder.FullName
+
+$gui = Join-Path $folder "CCEnhancer.exe.gui"
 New-Item $gui -type file -force
 
 #install start menu shortcut
 $programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
 $shortcutFilePath = Join-Path $programs $linkName 
-$targetPath = Join-Path $installDir $fileName
+$targetPath = Join-Path $folder $fileName
 Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePath -targetPath $targetPath
