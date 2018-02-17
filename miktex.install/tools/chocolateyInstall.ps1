b38b2393d8adee9b16546875cefbcbdb06e528a6
Wim Goedertier <33165762+goedertw@users.noreply.github.com>
2018-02-17 12:25:59 +0100

    fix for broken install path

--- miktex.install/tools/chocolateyInstall.ps1
+++ miktex.install/tools/chocolateyInstall.ps1
@@ -21,9 +21,9 @@ Function Get-RedirectedUrl {
       throw $_.Exception 
    }
 }
-$Url = Get-RedirectedURL http://mirrors.ctan.org/systems/win32/miktex/setup/basic-miktex-{{PackageVersion}}.exe
+$Url = Get-RedirectedURL http://mirrors.ctan.org/systems/win32/miktex/setup/windows-x86/basic-miktex-{{PackageVersion}}.exe
 
-$Url64 = Get-RedirectedURL http://mirrors.ctan.org/systems/win32/miktex/setup/basic-miktex-{{PackageVersion}}-x64.exe
+$Url64 = Get-RedirectedURL http://mirrors.ctan.org/systems/win32/miktex/setup/windows-x64/basic-miktex-{{PackageVersion}}-x64.exe
 
 
 
